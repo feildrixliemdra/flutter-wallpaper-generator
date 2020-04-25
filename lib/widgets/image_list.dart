6a3e494db024e5dfe2ec2e242ebdd9583e5ada4d
feildrixliemdra <feildrix.liemdra@ralali.com>
2020-04-25 15:34:27 +0700

    create project structure

---
+++ lib/widgets/image_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ImageList extends StatefulWidget {
+  @override
+  _ImageListState createState() => _ImageListState();
+}
+
+class _ImageListState extends State<ImageList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
